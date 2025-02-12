b98b2f7c46c9372e7dd2d950bb820b6a269d4b86
Mehemmed Kazimzade <mehemmedkazimzade54@gmail.com>
2025-02-12 21:05:58 +0400

    working on resizing not done

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,5 +1,28 @@
+import { useRef } from "react"
+
 export default function Editor() {
+    const scrollRef = useRef(null);
+    const resizableDiv = useRef(null);
+
+    const handleMouseDown = (e) => {
+        resize(e);
+    }
+
+    const resize = (e) => {
+        resizableDiv.current.style.width = e.clientX - resizableDiv.current.offsetLeft + "px";
+    }
+
     return <>
-        <h2>Editor</h2>
+        <div className="container">
+            <div ref={resizableDiv} className="code">
+                sdsjdkfj
+            </div>
+
+            <div ref={scrollRef} style={{width:"10px", height:"50vh", cursor:'ew-resize'}} onMouseDown={handleMouseDown}></div>
+            
+            <div className="result">
+                fjkdfjkdjfjkd
+            </div>
+        </div>
     </>
 }
